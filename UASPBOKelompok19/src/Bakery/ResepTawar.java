89aaf563ada09eb87ce892093ece946a9dbae883
Mawsali <111742299+Mawsali@users.noreply.github.com>
2023-06-26 22:11:40 +0700

    Adonan

--- UASPBOKelompok19/src/Bakery/ResepTawar.java
+++ UASPBOKelompok19/src/Bakery/ResepTawar.java
@@ -18,7 +18,14 @@ public class ResepTawar {
     public double Telur = 70;
     public double EsBatu = 300;
     public double BeratPcs = 400;
+    public double TotalBerat = 2065;
+    public double BeratAdonan = 400;
 
+     //Method Roti Per Batch
+    public double tawarPerBatch(){
+        return this.TotalBerat / this.BeratAdonan;
+    }
+    
     //Method Adonan
     public double adonanTepungTerigu() {
         return this.TepungTerigu / this.BeratPcs;
